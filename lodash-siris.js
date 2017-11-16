3d8ce45a87e2cc7b2905f55d3be1708e521fc775
Xavi Gimenez <xavi.gimenez@sirisacademic.com>
2017-11-16 10:48:24 +0100

    initial upload

---
+++ lodash-siris.js
@@ -0,0 +1,31 @@
+(function (_) {
+
+	if(_ == undefined)
+		return;
+	
+	// Code from : http://learnjsdata.com/combine_data.html
+	// combine data sets by one or more attributes. A way
+	// so simulate SQL joins if we consider javascript collections 
+	// as tables	
+	function join(lookupTable, mainTable, lookupKey, mainKey, select) {
+	    var l = lookupTable.length,
+	        m = mainTable.length,
+	        lookupIndex = [],
+	        output = [];
+	    for (var i = 0; i < l; i++) { // loop through l items
+	        var row = lookupTable[i];
+	        lookupIndex[row[lookupKey]] = row; // create an index for lookup table
+	    }
+	    for (var j = 0; j < m; j++) { // loop through m items
+	        var y = mainTable[j];
+	        var x = lookupIndex[y[mainKey]]; // get corresponding row from lookupTable
+	        output.push(select(y, x)); // select only the columns you need
+	    }
+	    return output;
+	};
+
+
+	// add mixins here
+	_.mixin( { 'join' : join });
+
+})(_);
