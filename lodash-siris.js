124bb9a9f96c9441ff651125b12f25f23ea87a59
Xavi Gimenez <xavi.gimenez@sirisacademic.com>
2017-11-16 12:42:59 +0100

    added trimToTimeFrame function

--- lodash-siris.js
+++ lodash-siris.js
@@ -40,7 +40,33 @@
 	};
 
 
+
+
+	// given two datasets that contain yearly data, filter them by the 
+	// yearly time frame common in both datasets
+	function trimToTimeFrame(dataset1, dataset2, key1, key2) {
+		var firstCommonYear = _.max(
+				_.min(_.map(dataset1, key1), key1),
+				_.min(_.map(dataset2, key2), key2)
+			),
+			lastCommonYear = _.min(
+				_.max(_.map(dataset1, key1), key1),
+				_.max(_.map(dataset2, key2), key2)
+			);
+
+		dataset1 = _.filter(dataset1, function(row) {
+			return _.inRange(row[key1], firstCommonYear, lastCommonYear)
+		});
+		dataset2 = _.filter(dataset2, function(row) {
+			return _.inRange(row[key2], firstCommonYear, lastCommonYear)
+		})
+	}
+
+
+
+
 	// add mixins here
 	_.mixin( { 'join' : join });
+	_.mixin( { 'trimToTimeFrame' : trimToTimeFrame });
 
 })(_);
